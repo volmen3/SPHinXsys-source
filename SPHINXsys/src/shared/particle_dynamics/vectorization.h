80d5093e8fbd0b3d564956541f032c574541789d
Evghenii Volodscoi <50427116+volmen3@users.noreply.github.com>
2022-09-28 00:24:03 +0200

    Update vectorization.h
    Backup: replace xsimd::batch::gather() with custom load-functions which return loaded RealBatchSph or VecdBatchSph (new)

--- SPHINXsys/src/shared/particle_dynamics/vectorization.h
+++ SPHINXsys/src/shared/particle_dynamics/vectorization.h
@@ -12,6 +12,95 @@
 #include "xsimd/xsimd.hpp"
 
 
+// TODO: put NTraits specialization in some other file, e.g. batch_sph.h
+#include "SimTKcommon/internal/NTraits.h"
+namespace SPH
+{
+	using RealBatchSph = xsimd::batch<Real>;
+
+	// Two dimensional SimTK::Vec where each element is of
+	// type xsimd::batch<Real>
+	using VecdBatchSph = SimTK::Vec<2, RealBatchSph>;
+}
+
+template <>
+class SimTK::NTraits<SPH::RealBatchSph>
+{
+public:                                         
+    typedef SPH::RealBatchSph    T;
+    typedef negator<T>       TNeg;              
+    typedef T                TWithoutNegator;   
+    typedef T                TReal;             
+    typedef T                TImag;             
+    typedef complex<T>       TComplex;          
+    typedef T                THerm;             
+    typedef T                TPosTrans;         
+    typedef T                TSqHermT;          
+    typedef T                TSqTHerm;          
+    typedef T                TElement;          
+    typedef T                TRow;              
+    typedef T                TCol;              
+    typedef T                TSqrt;             
+    typedef T                TAbs;              
+    typedef T                TStandard;         
+    typedef T                TInvert;           
+    typedef T                TNormalize;        
+    typedef T                Scalar;            
+    typedef T                ULessScalar;       
+    typedef T                Number;            
+    typedef T                StdNumber;         
+    typedef T                Precision;         
+    typedef T                ScalarNormSq;      
+
+	template <class P>
+	struct Result
+	{
+        typedef typename CNT<P>::template Result<T>::Mul Mul;
+        typedef typename CNT< typename CNT<P>::THerm >::template Result<T>::Mul Dvd;
+        typedef typename CNT<P>::template Result<T>::Add Add;
+        typedef typename CNT< typename CNT<P>::TNeg >::template Result<T>::Add Sub;
+    };                                          
+
+    enum
+    {
+        NRows = 1, 
+        NCols = 1, 
+        RowSpacing = 1, 
+        ColSpacing = 1, 
+        NPackedElements = 1, 
+        NActualElements = 1, 
+        NActualScalars = 1, 
+        ImagOffset = 0, 
+        RealStrideFactor = 1, 
+        ArgDepth = SCALAR_DEPTH, 
+        IsScalar = 1, 
+        IsULessScalar = 1, 
+        IsNumber = 1, 
+        IsStdNumber = 1, 
+        IsPrecision = 1, 
+        SignInterpretation = 1                 
+    };
+};
+
+template <>
+struct SimTK::Widest<SPH::RealBatchSph, SPH::RealBatchSph>
+{
+	typedef SPH::RealBatchSph Type;
+	typedef SPH::RealBatchSph Precision;
+};
+
+template<>
+struct SimTK::NTraits<SPH::RealBatchSph>::Result<SPH::RealBatchSph>
+{
+	typedef Widest<SPH::RealBatchSph, SPH::RealBatchSph>::Type Mul;
+	typedef Mul Dvd;
+	typedef Mul Add;
+	typedef Mul Sub;
+};
+
+template <>
+class SimTK::CNT<SPH::RealBatchSph> : public SimTK::NTraits<SPH::RealBatchSph> {};
+
 namespace SPH
 {
 	// Size of a SIMD register of SPH::Real values for default ISA (in bytes)
@@ -27,7 +116,45 @@ namespace SPH
 		reg_0 = xsimd::load_aligned(&vec_0[0]);
 	}
 
-	inline Vecd ViscousAccelerationInnerInteraction(
+	inline void InitWithDefaultValueVecdBatch(const Real default_value, VecdBatchSph& vec)
+	{
+		alignas(SIMD_REGISTER_SIZE_REAL_BYTES) Real vec_0[SIMD_REGISTER_SIZE_REAL_ELEMENTS] = { default_value };
+		vec = VecdBatchSph(xsimd::load_aligned(&vec_0[0]), xsimd::load_aligned(&vec_0[0]));
+	}
+
+	// Alternative to xsimd::batch<T,A>::gather() for gathering a RealBatchSph
+	inline RealBatchSph LoadReal(const size_t* idx, const StdLargeVec<Real>& indirect_indexed_data)
+	{
+		return
+		{
+			indirect_indexed_data[*idx],
+			indirect_indexed_data[*(idx + 1)],
+			indirect_indexed_data[*(idx + 2)],
+			indirect_indexed_data[*(idx + 3)]
+		};
+	}
+
+	// Alternative to xsimd::batch<T,A>::gather() for gathering a VecdBatchSph 
+	inline VecdBatchSph LoadVecd(const size_t* idx, const StdLargeVec<Vecd>& indirect_indexed_data)
+	{
+		return
+		{
+			{	// Batch X
+				indirect_indexed_data[*idx].get(0),
+				indirect_indexed_data[*(idx + 1)].get(0),
+				indirect_indexed_data[*(idx + 2)].get(0),
+				indirect_indexed_data[*(idx + 3)].get(0)
+			},
+			{	// Batch Y
+				indirect_indexed_data[*idx].get(1),
+				indirect_indexed_data[*(idx + 1)].get(1),
+				indirect_indexed_data[*(idx + 2)].get(1),
+				indirect_indexed_data[*(idx + 3)].get(1)
+			}
+		};
+	}
+
+	inline Vecd ViscousAccelerationInnerInteraction_Old(
 		const StdLargeVec<Vecd>& vel_, const Vecd& vel_i,
 		const StdLargeVec<Real>& r_ij_, const StdLargeVec<Real>& dW_ij_, const StdLargeVec<Real>& Vol_,
 		Real c_smoothing_length_, Real c_mu_rho_i,
@@ -35,35 +162,24 @@ namespace SPH
 	{
 		const std::size_t num_iter_simd = n_size - n_size % SIMD_REGISTER_SIZE_REAL_ELEMENTS;
 
-		// Initialization
-		xsimd::batch<Real> acceleration_vx, acceleration_vy;
-		InitWithDefaultValue(0.0, acceleration_vx);
-		InitWithDefaultValue(0.0, acceleration_vy);
+		VecdBatchSph acceleration_v;
+		InitWithDefaultValueVecdBatch(0.0, acceleration_v);
 
 		// Vectorized loop
 		for (size_t n = 0; n < num_iter_simd; n += SIMD_REGISTER_SIZE_REAL_ELEMENTS)
 		{
-			auto index = xsimd::load_unaligned(&j_[n]);
-			auto index_x = index * 2;
-			auto index_y = (index * 2) + 1;
-
-			auto Vol_v = xsimd::batch<Real, xsimd::default_arch>::gather(&Vol_[0], index);
-			auto vel_xv = xsimd::batch<Real, xsimd::default_arch>::gather(&vel_[0][0], index_x);
-			auto vel_yv = xsimd::batch<Real, xsimd::default_arch>::gather(&vel_[0][0], index_y);
-
-			auto dW_ij_v = xsimd::load_unaligned(&dW_ij_[n]);
-
-			auto vel_ixv = xsimd::batch<Real>(vel_i[0]);
-			auto vel_iyv = xsimd::batch<Real>(vel_i[1]);
-
+			auto Vol_v = LoadReal(&j_[n], Vol_);
+			auto vel_v = LoadVecd(&j_[n], vel_);
 			auto r_ij_v = xsimd::load_unaligned(&r_ij_[n]);
-			r_ij_v = r_ij_v + c_smoothing_length_;
+			auto dW_ij_v = xsimd::load_unaligned(&dW_ij_[n]);
+			auto vel_iv = VecdBatchSph({ vel_i[0] }, { vel_i[1] });
 
-			auto vel_derivative_vx = (vel_ixv - vel_xv) / r_ij_v;
-			auto vel_derivative_vy = (vel_iyv - vel_yv) / r_ij_v;
+			auto vel_derivative_v = (vel_iv - vel_v);
+			vel_derivative_v[0] = vel_derivative_v[0] / (r_ij_v + c_smoothing_length_);
+			vel_derivative_v[1] = vel_derivative_v[1] / (r_ij_v + c_smoothing_length_);
 
-			acceleration_vx = acceleration_vx + c_mu_rho_i * vel_derivative_vx * Vol_v * dW_ij_v;
-			acceleration_vy = acceleration_vy + c_mu_rho_i * vel_derivative_vy * Vol_v * dW_ij_v;
+			acceleration_v[0] = acceleration_v[0] + c_mu_rho_i * vel_derivative_v[0] * Vol_v * dW_ij_v;
+			acceleration_v[1] = acceleration_v[1] + c_mu_rho_i * vel_derivative_v[1] * Vol_v * dW_ij_v;
 		}
 
 		// Scalar loop
@@ -76,8 +192,8 @@ namespace SPH
 			acceleration_s = acceleration_s + c_mu_rho_i * vel_derivative_s * Vol_[index_j] * dW_ij_[n];
 		}
 
-		acceleration_s[0] = acceleration_s[0] + xsimd::reduce_add(acceleration_vx);
-		acceleration_s[1] = acceleration_s[1] + xsimd::reduce_add(acceleration_vy);
+		acceleration_s[0] = acceleration_s[0] + xsimd::reduce_add(acceleration_v[0]);
+		acceleration_s[1] = acceleration_s[1] + xsimd::reduce_add(acceleration_v[1]);
 
 		return acceleration_s;
 	}
